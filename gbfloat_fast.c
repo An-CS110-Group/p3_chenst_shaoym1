d8a0f20ba99eff67b2ac9e0f4abf38b80798c53c
caoster <caoster@icloud.com>
2022-05-19 00:38:23 +0800

    Maybe we can try thread pool tomorrow. OpenMP now takes a long time.

--- gbfloat_fast.c
+++ gbfloat_fast.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <immintrin.h>
 #include <math.h>
 #include <omp.h>
@@ -18,6 +17,7 @@
 #define PI 3.14159
 
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
 
 typedef struct FVec {
@@ -41,10 +41,8 @@ void normalize_FVec(FVec v) {
 }
 
 float *get_pixel(Image img, int x, int y) {
-    if (x < 0) { x = 0; }
-    if (x >= img.dimX) { x = img.dimX - 1; }
-    if (y < 0) { y = 0; }
-    if (y >= img.dimY) { y = img.dimY - 1; }
+    x = MIN(img.dimX - 1, MAX(x, 0));
+    y = MIN(img.dimY - 1, MAX(y, 0));
     return img.data + img.numChannels * (y * img.dimX + x);
 }
 
@@ -57,16 +55,11 @@ FVec make_gv(float a, float x0, float x1, int length, int min_length) {
     FVec v;
     v.length = length;
     v.min_length = min_length;
-    if (v.min_length > v.length) {
-        v.min_deta = 0;
-    } else {
-        v.min_deta = ((v.length - v.min_length) / 2);
-    }
+    v.min_deta = v.min_length > v.length ? 0 : (v.length - v.min_length) / 2;
     v.data = malloc(length * sizeof(float) + sizeof(float));
     v.sum = malloc((length / 2 + 1) * sizeof(float) + sizeof(float));
     float step = (x1 - x0) / ((float) length);
     int offset = length / 2;
-
     for (int i = 0; i < length; i++) { v.data[i] = gd(a, 0.0f, (float) (i - offset) * step); }
     normalize_FVec(v);
     return v;
@@ -88,26 +81,16 @@ void transpose_block(Image *src, Image *dst) {
 
 Image img_sc(Image a) {
     Image b = a;
-    b.data = malloc(b.dimX * b.dimY * b.numChannels * sizeof(float) + sizeof(float));
+    b.data = malloc(b.dimX * b.dimY * b.numChannels * sizeof(float) + 10 * sizeof(float));
     return b;
 }
 
-Image gb_h(Image a, FVec gv) {
+Image gb_h(Image a, FVec gv, float *gvData) {
     Image b = img_sc(a);
     int ext = gv.length / 2;
 
-    float gvData[3 * gv.length + 10];
-    #pragma omp parallel for schedule(dynamic) default(shared)
-    for (int i = 0; i < gv.length; ++i) {
-        gvData[3 * i + 0] = gv.data[i];
-        gvData[3 * i + 1] = gv.data[i];
-        gvData[3 * i + 2] = gv.data[i];
-    }
-
-    //    float *pixels = malloc((3 * a.dimX * a.dimY + 3 * a.dimX + 3 * gv.length) * sizeof(float));// Not to add extra space here in order to debug
     float *pixels = malloc(3 * (a.dimX + 2 * ext + 1) * a.dimY * sizeof(float));
-
-    #pragma omp parallel for schedule(dynamic) default(shared)
+#pragma omp parallel for schedule(dynamic) default(none) shared(a, pixels, ext)
     for (int j = 0; j < a.dimY; ++j) {
         for (int i = -ext; i < a.dimX + ext; ++i) {
             pixels[3 * i + 3 * ext + 3 * j * (a.dimX + 2 * ext + 1) + 0] = get_pixel(a, i, j)[0];
@@ -116,8 +99,7 @@ Image gb_h(Image a, FVec gv) {
         }
     }
 
-    //    #pragma omp parallel for schedule(dynamic) default(none) shared(a, b, gv, ext, gvData)
-    #pragma omp parallel for schedule(dynamic) default(shared)
+#pragma omp parallel for schedule(dynamic) default(none) shared(a, b, gv, ext, gvData, pixels)
     for (int y = 0; y < a.dimY; y++) {
         for (int x = 0; x < a.dimX; x++) {
             int deta = MIN(MIN(MIN(a.dimY - y - 1, y), MIN(a.dimX - x - 1, x)), gv.min_deta);
@@ -125,27 +107,9 @@ Image gb_h(Image a, FVec gv) {
             __m256 sum[3] = {_mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps()};
             int i;
             for (i = deta; i < gv.length - deta - 8; i += 8) {
-                sum[0] = _mm256_fmadd_ps(_mm256_loadu_ps(&pixels[3 * (x + i + 0) + 0 + 3 * y * (a.dimX + 2 * ext + 1)]), _mm256_loadu_ps(&gvData[3 * i + 0]),
-                                         sum[0]);
-                sum[1] = _mm256_fmadd_ps(_mm256_loadu_ps(&pixels[3 * (x + i + 2) + 2 + 3 * y * (a.dimX + 2 * ext + 1)]), _mm256_loadu_ps(&gvData[3 * i + 8]),
-                                         sum[1]);
-                sum[2] = _mm256_fmadd_ps(_mm256_loadu_ps(&pixels[3 * (x + i + 5) + 1 + 3 * y * (a.dimX + 2 * ext + 1)]), _mm256_loadu_ps(&gvData[3 * i + 16]),
-                                         sum[2]);
-
-                //                float *place[8] = {get_pixel(a, x - ext + i + 0, y), get_pixel(a, x - ext + i + 1, y), get_pixel(a, x - ext + i + 2, y),
-                //                                   get_pixel(a, x - ext + i + 3, y), get_pixel(a, x - ext + i + 4, y), get_pixel(a, x - ext + i + 5, y),
-                //                                   get_pixel(a, x - ext + i + 6, y), get_pixel(a, x - ext + i + 7, y)};
-                //                __attribute__((aligned(256))) float pixel[24] = {place[0][0], place[0][1], place[0][2], place[1][0], place[1][1], place[1][2],
-                //                                                                 place[2][0], place[2][1], place[2][2], place[3][0], place[3][1], place[3][2],
-                //                                                                 place[4][0], place[4][1], place[4][2], place[5][0], place[5][1], place[5][2],
-                //                                                                 place[6][0], place[6][1], place[6][2], place[7][0], place[7][1], place[7][2]};
-                //                sum[0] = _mm256_fmadd_ps(_mm256_loadu_ps(pixel + 0), _mm256_loadu_ps(&gvData[3 * i + 0]), sum[0]);
-                //                sum[1] = _mm256_fmadd_ps(_mm256_loadu_ps(pixel + 8), _mm256_loadu_ps(&gvData[3 * i + 8]), sum[1]);
-                //                sum[2] = _mm256_fmadd_ps(_mm256_loadu_ps(pixel + 16), _mm256_loadu_ps(&gvData[3 * i + 16]), sum[2]);
-                //
-                //                assert(*(&pixels[3 * (x + i + 0) + 0 + 3 * y * (a.dimX + 2 * ext + 1)]) == (pixel[0]));
-                //                assert(*(&pixels[3 * (x + i + 2) + 2 + 3 * y * (a.dimX + 2 * ext + 1)]) == (pixel[8]));
-                //                assert(*(&pixels[3 * (x + i + 5) + 1 + 3 * y * (a.dimX + 2 * ext + 1)]) == (pixel[16]));
+                sum[0] = _mm256_fmadd_ps(_mm256_loadu_ps(&pixels[3 * (x + i + 0) + 0 + 3 * y * (a.dimX + 2 * ext + 1)]), _mm256_loadu_ps(&gvData[3 * i + 0]), sum[0]);
+                sum[1] = _mm256_fmadd_ps(_mm256_loadu_ps(&pixels[3 * (x + i + 2) + 2 + 3 * y * (a.dimX + 2 * ext + 1)]), _mm256_loadu_ps(&gvData[3 * i + 8]), sum[1]);
+                sum[2] = _mm256_fmadd_ps(_mm256_loadu_ps(&pixels[3 * (x + i + 5) + 1 + 3 * y * (a.dimX + 2 * ext + 1)]), _mm256_loadu_ps(&gvData[3 * i + 16]), sum[2]);
             }
 
             for (; i < gv.length - deta; ++i) {
@@ -153,21 +117,25 @@ Image gb_h(Image a, FVec gv) {
                 fsum2 += gv.data[i] * get_pixel(a, x - ext + i, y)[1];
                 fsum3 += gv.data[i] * get_pixel(a, x - ext + i, y)[2];
             }
-            get_pixel(b, x, y)[0] =
-                    (sum[0][0] + sum[0][3] + sum[0][6] + sum[1][1] + sum[1][4] + sum[1][7] + sum[2][2] + sum[2][5] + fsum1) / gv.sum[ext - deta];
-            get_pixel(b, x, y)[1] =
-                    (sum[0][1] + sum[0][4] + sum[0][7] + sum[1][2] + sum[1][5] + sum[2][0] + sum[2][3] + sum[2][6] + fsum2) / gv.sum[ext - deta];
-            get_pixel(b, x, y)[2] =
-                    (sum[0][2] + sum[0][5] + sum[1][0] + sum[1][3] + sum[1][6] + sum[2][1] + sum[2][4] + sum[2][7] + fsum3) / gv.sum[ext - deta];
+            get_pixel(b, x, y)[0] = (sum[0][0] + sum[0][3] + sum[0][6] + sum[1][1] + sum[1][4] + sum[1][7] + sum[2][2] + sum[2][5] + fsum1) / gv.sum[ext - deta];
+            get_pixel(b, x, y)[1] = (sum[0][1] + sum[0][4] + sum[0][7] + sum[1][2] + sum[1][5] + sum[2][0] + sum[2][3] + sum[2][6] + fsum2) / gv.sum[ext - deta];
+            get_pixel(b, x, y)[2] = (sum[0][2] + sum[0][5] + sum[1][0] + sum[1][3] + sum[1][6] + sum[2][1] + sum[2][4] + sum[2][7] + fsum3) / gv.sum[ext - deta];
         }
     }
     return b;
 }
 
 Image apply_gb(Image a, FVec gv) {
-    Image b = gb_h(a, gv);
+    float gvData[3 * gv.length + 10];
+#pragma omp parallel for schedule(dynamic) default(none) shared(gv, gvData)
+    for (int i = 0; i < gv.length; ++i) {
+        gvData[3 * i + 0] = gv.data[i];
+        gvData[3 * i + 1] = gv.data[i];
+        gvData[3 * i + 2] = gv.data[i];
+    }
+    Image b = gb_h(a, gv, gvData);
     transpose_block(&b, &a);
-    b = gb_h(a, gv);
+    b = gb_h(a, gv, gvData);
     Image c = img_sc(a);
     transpose_block(&b, &c);
     free(b.data);
@@ -194,7 +162,6 @@ int main(int argc, char **argv) {
 
     FVec v = make_gv(a, x0, x1, dim, min_dim);
 
-    //    print_fvec(v);
     Image img;
     img.data = stbi_loadf(argv[1], &(img.dimX), &(img.dimY), &(img.numChannels), 0);
 
@@ -271,11 +238,12 @@ int main(int argc, char **argv) {
 
 // Reply to @sirius: All above are carefully tested, some of which helps a lot!
 //
-// FIXME: gb_v is SUPER slow! Much slower than expected due to cache miss issues.
+// FIXED: gb_v is SUPER slow! Much slower than expected due to cache miss issues.
 //
 // DONE: transpose matrix in gb_v
 //      ABORT: Speedup transpose
 //          Comment: No need to speedup, it takes a very small portion of program.
-// TODO: change sequence of loop in gb_v
-// TODO: try __mm256, it shall be fast on Autolab.
+// ABORT: change sequence of loop in gb_v
+//          Comment: Transposing is far faster than expected.
+// DONE: try __mm256, it shall be fast on Autolab.
 // DONE: test with a 4999x4999 pic
