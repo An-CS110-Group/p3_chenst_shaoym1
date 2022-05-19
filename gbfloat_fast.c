dc50d99af9444733d743edc0af8805ff148440b1
caoster <caoster@icloud.com>
2022-05-19 22:18:56 +0800

    Try get_nprocs_conf

--- gbfloat_fast.c
+++ gbfloat_fast.c
@@ -1,16 +1,35 @@
 #include <assert.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
 #include <omp.h>
+#include <pthread.h>
+#include <stdio.h>
 #include <sys/sysinfo.h>
+#include <unistd.h>
 
 #define assert__(x) for (; !(x); assert(x))
 
 
 int main(int argc, char **argv) {
     printf("%d %d", get_nprocs_conf(), get_nprocs());
-    assert(get_nprocs_conf() <= 4);
+    assert(get_nprocs_conf() >=1);
+    assert(get_nprocs_conf() >=2);
+    assert(get_nprocs_conf() >=3);
+    assert(get_nprocs_conf() >=4);
+    assert(get_nprocs_conf() >=5);
+    assert(get_nprocs_conf() >=6);
+    assert(get_nprocs_conf() >=7);
+    assert(get_nprocs_conf() >=8);
+    assert(get_nprocs_conf() >=9);
+    assert(get_nprocs_conf() >=10);
+    assert(get_nprocs_conf() >=11);
+    assert(get_nprocs_conf() >=12);
+    assert(get_nprocs_conf() >=13);
+    assert(get_nprocs_conf() >=14);
+    assert(get_nprocs_conf() >=15);
+    assert(get_nprocs_conf() >=16);
+    assert(get_nprocs_conf() >=17);
+    assert(get_nprocs_conf() >=18);
+
+
     assert(get_nprocs() == 14);
 
 
