b5b2e0c185251b51bfb6fda96b6d302265932e44
caoster <caoster@icloud.com>
2022-05-19 21:17:17 +0800

    Let's see whether we can detect how many threads we can use.

--- gbfloat_fast.c
+++ gbfloat_fast.c
@@ -5,10 +5,13 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
+#include <unistd.h>
 #include <xmmintrin.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
+#include "thpool.h"
 
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -32,6 +35,8 @@ typedef struct Image {
     float *data;
 } Image;
 
+threadpool thPool;
+
 void normalize_FVec(FVec v) {
     unsigned int i, j;
     int ext = v.length / 2;
@@ -39,12 +44,12 @@ void normalize_FVec(FVec v) {
     for (i = ext + 1, j = 1; i < v.length; i++, j++) { v.sum[j] = v.sum[j - 1] + v.data[i] * 2; }
 }
 
-float *get_pixel(Image img, int x, int y) {
+float *get_pixel(Image *img, int x, int y) {
     if (x < 0) { x = 0; }
-    if (x >= img.dimX) { x = img.dimX - 1; }
+    if (x >= img->dimX) { x = img->dimX - 1; }
     if (y < 0) { y = 0; }
-    if (y >= img.dimY) { y = img.dimY - 1; }
-    return img.data + img.numChannels * (y * img.dimX + x);
+    if (y >= img->dimY) { y = img->dimY - 1; }
+    return img->data + img->numChannels * (y * img->dimX + x);
 }
 
 float gd(float a, float b, float x) {
@@ -71,18 +76,31 @@ FVec make_gv(float a, float x0, float x1, int length, int min_length) {
     return v;
 }
 
+typedef struct transExeUnit {
+    Image *src;
+    Image *dst;
+    int i;
+} transExeUnit;
+
+void processTrans(transExeUnit *var) {
+    for (int j = 0; j < var->src->dimY; ++j) {
+        var->dst->data[(var->dst->dimX * var->i + j) * var->dst->numChannels + 0] = var->src->data[(var->src->dimX * j + var->i) * var->src->numChannels + 0];
+        var->dst->data[(var->dst->dimX * var->i + j) * var->dst->numChannels + 1] = var->src->data[(var->src->dimX * j + var->i) * var->src->numChannels + 1];
+        var->dst->data[(var->dst->dimX * var->i + j) * var->dst->numChannels + 2] = var->src->data[(var->src->dimX * j + var->i) * var->src->numChannels + 2];
+    }
+}
+
 void transpose_block(Image *src, Image *dst) {
     dst->dimX = src->dimY;
     dst->dimY = src->dimX;
     dst->numChannels = src->numChannels;
-#pragma omp parallel for schedule(nonmonotonic:static) default(none) shared(src, dst)
+    //#pragma omp parallel for schedule(dynamic) default(none) shared(src, dst)
     for (int i = 0; i < src->dimX; ++i) {
-        for (int j = 0; j < src->dimY; ++j) {
-            dst->data[(dst->dimX * i + j) * dst->numChannels + 0] = src->data[(src->dimX * j + i) * src->numChannels + 0];
-            dst->data[(dst->dimX * i + j) * dst->numChannels + 1] = src->data[(src->dimX * j + i) * src->numChannels + 1];
-            dst->data[(dst->dimX * i + j) * dst->numChannels + 2] = src->data[(src->dimX * j + i) * src->numChannels + 2];
-        }
+        processTrans(&(transExeUnit){.src = src, .dst = dst, .i = i});
+        //        There could be data race if use thpool
+        //        thpool_add_work(thPool, (void (*)(void *)) processTrans, &(transExeUnit){.src=src, .dst=dst, .i=i});
     }
+    thpool_wait(thPool);
 }
 
 Image img_sc(Image a) {
@@ -91,59 +109,77 @@ Image img_sc(Image a) {
     return b;
 }
 
-Image gb_h(Image a, FVec gv, float *gvData) {
-    Image b = img_sc(a);
+typedef struct exeUnit {
+    int y;
+    Image *a;
+    Image *b;
+    FVec *gv;
+    int ext;
+    float *gvData;
+    float *pixels;
+} exeUnit;
+
+void thread(exeUnit *var) {
+    for (int x = 0; x < var->a->dimX; x++) {
+        int deta = MIN(MIN(MIN(var->a->dimY - var->y - 1, var->y), MIN(var->a->dimX - x - 1, x)), var->gv->min_deta);
+        __m256 sum[3] = {_mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps()};
+        int i;
+        for (i = deta; i < var->gv->length - deta - 8; i += 8) {
+            sum[0] = _mm256_fmadd_ps(_mm256_loadu_ps(&var->pixels[3 * (x + i + 0) + 0 + 3 * var->y * (var->a->dimX + 2 * var->ext + 1)]), _mm256_loadu_ps(&var->gvData[3 * i + 0]),
+                                     sum[0]);
+            sum[1] = _mm256_fmadd_ps(_mm256_loadu_ps(&var->pixels[3 * (x + i + 2) + 2 + 3 * var->y * (var->a->dimX + 2 * var->ext + 1)]), _mm256_loadu_ps(&var->gvData[3 * i + 8]),
+                                     sum[1]);
+            sum[2] = _mm256_fmadd_ps(_mm256_loadu_ps(&var->pixels[3 * (x + i + 5) + 1 + 3 * var->y * (var->a->dimX + 2 * var->ext + 1)]), _mm256_loadu_ps(&var->gvData[3 * i + 16]),
+                                     sum[2]);
+        }
+        float fsum1 = 0, fsum2 = 0, fsum3 = 0;
+        for (; i < var->gv->length - deta; ++i) {
+            fsum1 += var->gv->data[i] * get_pixel(var->a, x - var->ext + i, var->y)[0];
+            fsum2 += var->gv->data[i] * get_pixel(var->a, x - var->ext + i, var->y)[1];
+            fsum3 += var->gv->data[i] * get_pixel(var->a, x - var->ext + i, var->y)[2];
+        }
+        get_pixel(var->b, x, var->y)[0] = (sum[0][0] + sum[0][3] + sum[0][6] + sum[1][1] + sum[1][4] + sum[1][7] + sum[2][2] + sum[2][5] + fsum1) / var->gv->sum[var->ext - deta];
+        get_pixel(var->b, x, var->y)[1] = (sum[0][1] + sum[0][4] + sum[0][7] + sum[1][2] + sum[1][5] + sum[2][0] + sum[2][3] + sum[2][6] + fsum2) / var->gv->sum[var->ext - deta];
+        get_pixel(var->b, x, var->y)[2] = (sum[0][2] + sum[0][5] + sum[1][0] + sum[1][3] + sum[1][6] + sum[2][1] + sum[2][4] + sum[2][7] + fsum3) / var->gv->sum[var->ext - deta];
+    }
+}
+
+Image gb_h(Image *a, FVec gv, float *gvData) {
+    Image b = img_sc(*a);
     int ext = gv.length / 2;
 
-    float *pixels = aligned_alloc(256, (((3 * (a.dimX + 2 * ext + 1) * a.dimY * sizeof(float)) >> 9) + 1) << 9);
+    float *pixels = malloc(3 * (a->dimX + 2 * ext + 1) * a->dimY * sizeof(float));
 
-#pragma omp parallel for schedule(static) default(none) shared(a, ext, pixels)
-    for (int j = 0; j < a.dimY; ++j) {
-        for (int i = -ext; i < a.dimX + ext; ++i) {
-            pixels[3 * i + 3 * ext + 3 * j * (a.dimX + 2 * ext + 1) + 0] = get_pixel(a, i, j)[0];
-            pixels[3 * i + 3 * ext + 3 * j * (a.dimX + 2 * ext + 1) + 1] = get_pixel(a, i, j)[1];
-            pixels[3 * i + 3 * ext + 3 * j * (a.dimX + 2 * ext + 1) + 2] = get_pixel(a, i, j)[2];
+    //#pragma omp parallel for schedule(dynamic) default(none) shared(a, ext, pixels)
+    for (int j = 0; j < a->dimY; ++j) {
+        for (int i = -ext; i < a->dimX + ext; ++i) {
+            pixels[3 * i + 3 * ext + 3 * j * (a->dimX + 2 * ext + 1) + 0] = get_pixel(a, i, j)[0];
+            pixels[3 * i + 3 * ext + 3 * j * (a->dimX + 2 * ext + 1) + 1] = get_pixel(a, i, j)[1];
+            pixels[3 * i + 3 * ext + 3 * j * (a->dimX + 2 * ext + 1) + 2] = get_pixel(a, i, j)[2];
         }
     }
 
-#pragma omp parallel for schedule(nonmonotonic:dynamic) default(none) shared(a, b, gv, ext, gvData, pixels)
-    for (int y = 0; y < a.dimY; y++) {
-        for (int x = 0; x < a.dimX; x++) {
-            int deta = MIN(MIN(MIN(a.dimY - y - 1, y), MIN(a.dimX - x - 1, x)), gv.min_deta);
-            float fsum1 = 0, fsum2 = 0, fsum3 = 0;
-            __m256 sum[3] = {_mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps()};
-            int i;
-            for (i = deta; i < gv.length - deta - 8; i += 8) {
-                sum[0] = _mm256_fmadd_ps(_mm256_loadu_ps(&pixels[3 * (x + i + 0) + 0 + 3 * y * (a.dimX + 2 * ext + 1)]), _mm256_load_ps(&gvData[3 * i + 0]), sum[0]);
-                sum[1] = _mm256_fmadd_ps(_mm256_loadu_ps(&pixels[3 * (x + i + 2) + 2 + 3 * y * (a.dimX + 2 * ext + 1)]), _mm256_load_ps(&gvData[3 * i + 8]), sum[1]);
-                sum[2] = _mm256_fmadd_ps(_mm256_loadu_ps(&pixels[3 * (x + i + 5) + 1 + 3 * y * (a.dimX + 2 * ext + 1)]), _mm256_load_ps(&gvData[3 * i + 16]), sum[2]);
-            }
-
-            for (; i < gv.length - deta; ++i) {
-                fsum1 += gv.data[i] * get_pixel(a, x - ext + i, y)[0];
-                fsum2 += gv.data[i] * get_pixel(a, x - ext + i, y)[1];
-                fsum3 += gv.data[i] * get_pixel(a, x - ext + i, y)[2];
-            }
-            get_pixel(b, x, y)[0] = (sum[0][0] + sum[0][3] + sum[0][6] + sum[1][1] + sum[1][4] + sum[1][7] + sum[2][2] + sum[2][5] + fsum1) / gv.sum[ext - deta];
-            get_pixel(b, x, y)[1] = (sum[0][1] + sum[0][4] + sum[0][7] + sum[1][2] + sum[1][5] + sum[2][0] + sum[2][3] + sum[2][6] + fsum2) / gv.sum[ext - deta];
-            get_pixel(b, x, y)[2] = (sum[0][2] + sum[0][5] + sum[1][0] + sum[1][3] + sum[1][6] + sum[2][1] + sum[2][4] + sum[2][7] + fsum3) / gv.sum[ext - deta];
-        }
+    //#pragma omp parallel for schedule(dynamic) default(none) shared(a, b, gv, ext, gvData, pixels)
+    for (int y = 0; y < a->dimY; ++y) {
+        //        thread(&work);
+        thpool_add_work(thPool, (void (*)(void *)) thread, &(exeUnit){.y = y, .a = a, .b = &b, .gv = &gv, .ext = ext, .gvData = gvData, .pixels = pixels});
     }
+    thpool_wait(thPool);
     free(pixels);
     return b;
 }
 
 Image apply_gb(Image a, FVec gv) {
-    __attribute__((aligned(256))) float gvData[3 * gv.length + 10];
-#pragma omp parallel for schedule(nonmonotonic:static) default(none) shared(gv, gvData)
+    __attribute__((aligned(64))) float gvData[3 * gv.length + 10];
+    //#pragma omp parallel for schedule(dynamic) default(none) shared(gv, gvData)
     for (int i = 0; i < gv.length; ++i) {
         gvData[3 * i + 0] = gv.data[i];
         gvData[3 * i + 1] = gv.data[i];
         gvData[3 * i + 2] = gv.data[i];
     }
-    Image b = gb_h(a, gv, gvData);
+    Image b = gb_h(&a, gv, gvData);
     transpose_block(&b, &a);
-    b = gb_h(a, gv, gvData);
+    b = gb_h(&a, gv, gvData);
     Image c = img_sc(a);
     transpose_block(&b, &c);
     free(b.data);
@@ -151,55 +187,16 @@ Image apply_gb(Image a, FVec gv) {
     return c;
 }
 
+
 int main(int argc, char **argv) {
-    struct timeval start_time, stop_time, elapsed_time;
-    gettimeofday(&start_time, NULL);
-    if (argc < 6) {
-        printf("Usage: ./gb.exe <inputjpg> <outputname> <float: a> <float: x0> <float: x1> <unsigned int: dim>\n");
-        exit(0);
-    }
-
-    float a, x0, x1;
-    int dim, min_dim;
-
-    sscanf(argv[3], "%f", &a);       /* 0.6 */
-    sscanf(argv[4], "%f", &x0);      /* -2.0 */
-    sscanf(argv[5], "%f", &x1);      /* 2.0 */
-    sscanf(argv[6], "%d", &dim);     /* 1001 */
-    sscanf(argv[7], "%d", &min_dim); /* 201 */
-
-    FVec v = make_gv(a, x0, x1, dim, min_dim);
-
-    //    print_fvec(v);
-    Image img;
-    img.data = stbi_loadf(argv[1], &(img.dimX), &(img.dimY), &(img.numChannels), 0);
-
-    Image imgOut = apply_gb(img, v);
-    stbi_write_jpg(argv[2], imgOut.dimX, imgOut.dimY, imgOut.numChannels, imgOut.data, 90);
-    gettimeofday(&stop_time, NULL);
-    timersub(&stop_time, &start_time, &elapsed_time);
-    printf("%f \n", (double) elapsed_time.tv_sec + (double) elapsed_time.tv_usec / 1000000.0);
-    free(imgOut.data);
-    free(v.data);
-    free(v.sum);
+    assert(sysconf(_SC_NPROCESSORS_ONLN) >= 4);
+    assert(sysconf(_SC_NPROCESSORS_ONLN) == 4);
     return 0;
 }
 
 
 /*.......................Some notes.........................................*/
 
-/*can we parallel manually instesd of using #pragma omp parallel for schedule(dynamic) default(none) private(y) shared(a, x, b, pc, gv, ext, deta, i)*/
-//somethinglike
-// #pragma omp parallel {
-// int id, i, Nthreads, start, end;
-// id = omp_get_thread_num();
-// Nthreads = omp_get_num_threads();
-// start = id * N / Nthreads;
-// end = (id + 1) * N / Nthreads;
-// for (i = start; i < end; i++) {
-// a[i] = a[i] + b[i];
-// }
-// }
 
 //dynamic is very useful instead of choosing static #pragma omp parallel for schedule(dynamic)
 //because the time of each iteration is inbalancing
