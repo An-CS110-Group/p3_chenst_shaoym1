531b6ee0863ee5599324fdd7e85b616fa8f2895a
caoster <caoster@icloud.com>
2022-05-19 21:50:44 +0800

    Final assertion about _SC_THREAD_THREADS_MAX.

--- gbfloat_fast.c
+++ gbfloat_fast.c
@@ -6,7 +6,7 @@
 
 
 int main(int argc, char **argv) {
-    assert(sysconf(_SC_THREAD_THREADS_MAX) == 4);
+    assert(sysconf(_SC_THREAD_THREADS_MAX) < 4);
     assert(sysconf(_SC_THREADS) == 4);
     return 0;
 }
