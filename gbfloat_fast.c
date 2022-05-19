fdaf1a07550856a2ca3ba7076133ca129693652f
caoster <caoster@icloud.com>
2022-05-19 21:42:47 +0800

    New assertions about threads.

--- gbfloat_fast.c
+++ gbfloat_fast.c
@@ -6,13 +6,14 @@
 
 
 int main(int argc, char **argv) {
-    assert(sysconf(_SC_NPROCESSORS_CONF) > 4);
-    assert(sysconf(_SC_NPROCESSORS_ONLN) > 4);
-    assert(sysconf(_SC_NPROCESSORS_CONF) > 8);
-    assert(sysconf(_SC_NPROCESSORS_ONLN) > 8);
-    assert(sysconf(_SC_NPROCESSORS_CONF) > 12);
-    assert(sysconf(_SC_NPROCESSORS_ONLN) > 12);
-    assert(sysconf(_SC_NPROCESSORS_CONF) > 16);
-    assert(sysconf(_SC_NPROCESSORS_ONLN) > 16);
+    assert(sysconf(_SC_THREAD_THREADS_MAX) > 4);
+    //    assert(sysconf(_SC_NPROCESSORS_ONLN) > 4);
+    assert(sysconf(_SC_THREAD_THREADS_MAX) > 8);
+    //    assert(sysconf(_SC_NPROCESSORS_ONLN) > 8);
+    assert(sysconf(_SC_THREAD_THREADS_MAX) > 12);
+    //    assert(sysconf(_SC_NPROCESSORS_ONLN) > 12);
+    assert(sysconf(_SC_THREAD_THREADS_MAX) > 16);
+    //    assert(sysconf(_SC_NPROCESSORS_ONLN) > 16);
+    assert(sysconf(_SC_THREAD_THREADS_MAX) > 20);
     return 0;
 }
