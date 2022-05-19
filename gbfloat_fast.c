881d3fbdec29102961ffce2786f546eac6ab446e
caoster <caoster@icloud.com>
2022-05-19 22:30:20 +0800

    Try get_nprocs_conf

--- gbfloat_fast.c
+++ gbfloat_fast.c
@@ -10,24 +10,25 @@
 
 int main(int argc, char **argv) {
     printf("%d %d", get_nprocs_conf(), get_nprocs());
-    assert(get_nprocs_conf() >=1);
-    assert(get_nprocs_conf() >=2);
-    assert(get_nprocs_conf() >=3);
-    assert(get_nprocs_conf() >=4);
-    assert(get_nprocs_conf() >=5);
-    assert(get_nprocs_conf() >=6);
-    assert(get_nprocs_conf() >=7);
-    assert(get_nprocs_conf() >=8);
-    assert(get_nprocs_conf() >=9);
-    assert(get_nprocs_conf() >=10);
-    assert(get_nprocs_conf() >=11);
-    assert(get_nprocs_conf() >=12);
-    assert(get_nprocs_conf() >=13);
-    assert(get_nprocs_conf() >=14);
-    assert(get_nprocs_conf() >=15);
-    assert(get_nprocs_conf() >=16);
-    assert(get_nprocs_conf() >=17);
-    assert(get_nprocs_conf() >=18);
+    assert(get_nprocs() >= 1);
+    assert(get_nprocs() >= 2);
+    assert(get_nprocs() >= 3);
+    assert(get_nprocs() >= 4);
+    assert(get_nprocs() >= 5);
+    assert(get_nprocs() >= 6);
+    assert(get_nprocs() >= 7);
+    assert(get_nprocs() >= 8);
+    assert(get_nprocs() >= 9);
+    assert(get_nprocs() >= 10);
+    assert(get_nprocs() >= 11);
+    assert(get_nprocs() >= 12);
+    assert(get_nprocs() >= 13);
+    assert(get_nprocs() >= 14);
+    assert(get_nprocs() >= 15);
+    assert(get_nprocs() >= 16);
+    assert(get_nprocs() >= 17);
+    assert(get_nprocs() >= 18);
+    assert(get_nprocs() <= 50);
 
 
     assert(get_nprocs() == 14);
