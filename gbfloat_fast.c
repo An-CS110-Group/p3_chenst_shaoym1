cb105ae977ab384ac4d6c190900c8e8f566a39d0
caoster <caoster@icloud.com>
2022-05-19 21:28:50 +0800

    New assertions

--- gbfloat_fast.c
+++ gbfloat_fast.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-    assert(sysconf(_SC_NPROCESSORS_ONLN) >= 4);
-    assert(sysconf(_SC_NPROCESSORS_ONLN) == 4);
+    assert(sysconf(_SC_NPROCESSORS_ONLN) == 28);
+    assert(sysconf(_SC_NPROCESSORS_ONLN) == 20);
     return 0;
 }
