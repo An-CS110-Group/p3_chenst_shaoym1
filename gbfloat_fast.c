ded7cd15fb0da2266bca7b7b24f7e721bf2fcbd5
caoster <caoster@icloud.com>
2022-05-19 21:57:13 +0800

    Try omp_get_max_threads

--- gbfloat_fast.c
+++ gbfloat_fast.c
@@ -1,12 +1,18 @@
 #include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <omp.h>
 
 #define assert__(x) for (; !(x); assert(x))
 
 
 int main(int argc, char **argv) {
-    assert(sysconf(_SC_THREAD_THREADS_MAX) < 4);
-    assert(sysconf(_SC_THREADS) == 4);
+    assert(sysconf(_SC_THREADS) != 4);
+    assert(sysconf(_SC_THREAD_SAFE_FUNCTIONS) != 4);
+    assert(sysconf(_SC_THREAD_THREADS_MAX) != 4);
+    assert(omp_get_max_threads() == 4);
+
+
     return 0;
 }
