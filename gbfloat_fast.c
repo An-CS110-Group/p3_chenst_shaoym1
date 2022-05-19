e1a5f6ecb3a94d19def4335f31d47f6c618b58fc
caoster <caoster@icloud.com>
2022-05-19 21:39:08 +0800

    New assertions about cores.

--- gbfloat_fast.c
+++ gbfloat_fast.c
@@ -1,8 +1,18 @@
 #include <assert.h>
+#include <stdio.h>
 #include <unistd.h>
 
+#define assert__(x) for (; !(x); assert(x))
+
+
 int main(int argc, char **argv) {
-    assert(sysconf(_SC_NPROCESSORS_CONF) == 14);
-    assert(sysconf(_SC_NPROCESSORS_CONF) == 10);
+    assert(sysconf(_SC_NPROCESSORS_CONF) > 4);
+    assert(sysconf(_SC_NPROCESSORS_ONLN) > 4);
+    assert(sysconf(_SC_NPROCESSORS_CONF) > 8);
+    assert(sysconf(_SC_NPROCESSORS_ONLN) > 8);
+    assert(sysconf(_SC_NPROCESSORS_CONF) > 12);
+    assert(sysconf(_SC_NPROCESSORS_ONLN) > 12);
+    assert(sysconf(_SC_NPROCESSORS_CONF) > 16);
+    assert(sysconf(_SC_NPROCESSORS_ONLN) > 16);
     return 0;
 }
